37da01fe30aa7f59e549fdb1619814d5be417fb5
Kapil Garg <Kapil_Garg@Kapils-MacBook-Pro.local>
2012-11-15 13:35:44 -0600

    Initial Commit

---
+++ DeadliestWarrior/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DeadliestWarrior
+//
+//  Created by Kapil Garg on 11/15/12.
+//  Copyright (c) 2012 KapilAndrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
